002181cb97dc098a2546bc2748f18e66ceb580b6
Anne Lagarde <anne.lagarde@hetic.net>
2019-06-04 16:55:24 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -70,7 +70,7 @@
             login(email, pwd) {
                 axios({
                     method: 'post',
-                    url: 'http://ulysse.idequanet.com/ben/web/api/user/login',
+                    url: 'https://ulysse.idequanet.com/ben/web/api/user/login',
                     data: {
                         user: {
                             email: email,
